85756b912f54e4bf5eef330a292ad457414dfaba
Lucas Mariano Da Silva <lucasm1593@gmail.com>
2023-11-05 13:43:39 -0300

    Subindo as musicas

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 const player = document.querySelector("#player");
 const musicName = document.querySelector("#musicName");
-const playerPauseButton = document.querySelector("#playPauseButton");
+const playPauseButton = document.querySelector("#playPauseButton");
 const prevButton = document.querySelector("#prevButton");
+const nextButton = document.querySelector("#nextButton");
 const currentTime = document.querySelector("#currentTime");
 const duration = document.querySelector("#duration");
 const progressBar = document.querySelector(".progress-bar");
@@ -9,19 +10,66 @@ const progress = document.querySelector(".progress");
 
 import songs from "./songs.js";
 
-const textButtonPlay = "<1 class = 'bx bx-caret-right'></i>";
-const textButtonPause = "<1 class = 'bx bx-pause'></i>";
+const textButtonPlay = "<i class='bx bx-caret-right'></i>";
+const textButtonPause = "<i class='bx bx-pause'></i>";
 
 let index = 0;
 
 prevButton.onclick = () => prevNextMusic("prev");
 nextButton.onclick = () => prevNextMusic();
 
-const prevNextMusic = (type = "next") =>{
-    if ((type == "next" && index + 1 == songs.lenght)  || 
-    type == "init"){
-        index = 0;
-    }
+playPauseButton.onclick = () => playPause();
+
+const playPause = () => {
+  if (player.paused) {
+    player.play();
+    playPauseButton.innerHTML = textButtonPause;
+  } else {
+    player.pause();
+    playPauseButton.innerHTML = textButtonPlay;
+  }
+};
+
+player.ontimeupdate = () => updateTime();
+
+const updateTime = () => {
+  const currentMinutes = Math.floor(player.currentTime / 60);
+  const currentSeconds = Math.floor(player.currentTime % 60);
+  currentTime.textContent = currentMinutes + ":" + formatZero(currentSeconds);
+
+  const durationFormatted = isNaN(player.duration) ? 0 : player.duration;
+  const durationMinutes = Math.floor(durationFormatted / 60);
+  const durationSeconds = Math.floor(durationFormatted % 60);
+  duration.textContent = durationMinutes + ":" + formatZero(durationSeconds);
+
+  const progressWidth = durationFormatted
+    ? (player.currentTime / durationFormatted) * 100
+    : 0;
+
+  progress.style.width = progressWidth + "%";
+};
+
+const formatZero = (n) => (n < 10 ? "0" + n : n);
+
+progressBar.onclick = (e) => {
+  const newTime = (e.offsetX / progressBar.offsetWidth) * player.duration;
+  player.currentTime = newTime;
+};
+
+const prevNextMusic = (type = "next") => {
+  if ((type == "next" && index + 1 === songs.length) || type === "init") {
+    index = 0;
+  } else if (type == "prev" && index === 0) {
+    index = songs.length;
+  } else {
+    index = type === "prev" && index ? index - 1 : index + 1;
+  }
+
+  player.src = songs[index].src;
+  musicName.innerHTML = songs[index].name;
+  if (type !== "init") playPause();
+
+  updateTime();
 };
 
 prevNextMusic("init");
